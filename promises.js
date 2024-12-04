b995d97d10badfb234346a68c354fe139c3d974f
Hi <azamatielts987@gmail.com>
2024-12-04 22:46:52 +0500

    homework classes

--- promises.js
+++ promises.js
@@ -500,3 +500,10 @@
 
 //---------------------- delay promise ------
 
+new Promise(function(resolve, reject) {
+    setTimeout(() => resolve(1), 1000)
+}).then(function(result) {
+    console.log(result);
+    
+    return new Promise((resolve, reject) )
+})
